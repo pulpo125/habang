77238c6cd3d5fc1d4f7d6cf0c9e003997225e8e8
kang.pulpo <kang.pulpo@gmail.com>
2023-05-09 12:06:59 +0900

    today's movie 완성

--- movie_pj/Analysis.py
+++ movie_pj/Analysis.py
@@ -4,22 +4,23 @@ import pandas as pd
 # 데이터 로드
 df = pd.read_csv('data/movie_eda.csv')
 
+### Sidebar
 with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
+    st.subheader("How to use")
+    st.markdown("""<span style='color:grey'>IMDB Top 250 영화 데이터를 이용하여<span/><br/>
+    <span>년도별, 장르별, 감독별로 순위에<span/><br/>
+    <span>가장 많이 랭킹된 Top 5를 분석합니다.:smile:<span/><br/>
+    <span>컬럼을 선택하고 자세히 보고 싶은 데이터를 선택한 후<span/><br/>
+    <span>상세정보가 궁금한 영화의 순위를 검색창에 입력해주세요.<span/><br/>
+    <hr style='margin: 0px; border-bottom: 3px dashed rgba(49, 51, 63, 0.2);'>""", unsafe_allow_html=True)
+
+with st.sidebar:
+    group_by_option = st.selectbox('컬럼을 선택하세요: ', ['None'] + ['year'] + ['genre'] + ['director_name'])
 
 # title
-st.image('https://static.amazon.jobs/teams/53/images/IMDb_Header_Page.jpg?1501027252')
 st.title(':movie_camera: IMDB Top 250 Movies')
 
-st.divider()
-
 #1: 컬럼별 Top 5
-st.header('컬럼별 Top 5')
-group_by_option = st.selectbox('Select the column : ', ['None'] + ['year'] + ['genre'] + ['director_name'])
-
 cols = 'None'
 if group_by_option == 'None':
     tmp_df = df[['title', 'imbd_votes', 'imbd_rating']]
@@ -27,42 +28,32 @@ if group_by_option == 'None':
     tmp_df.index.name = 'rank'
     st.dataframe(tmp_df, width=700)
 else:
+    st.subheader(f'{group_by_option}별 가장 많이 랭킹된 Top 5')
     tmp_df = df[group_by_option].value_counts()[:5].reset_index()
     tmp_df = pd.DataFrame(tmp_df).rename(columns={group_by_option:'count', 'index':group_by_option})
     st.bar_chart(tmp_df, x = group_by_option, y = 'count')
 
-    st.divider()
-
     group_list = tmp_df[group_by_option].to_list()
-    cols = st.selectbox('Select the data: ', ['None'] + list(group_list))
+    with st.sidebar:
+        cols = st.selectbox('데이터를 선택해주세요: ', ['None'] + list(group_list))
 
 #2: 1에서 선택한 데이터 보여주기
 if cols == 'None':
     st.empty()
 
 else:
+    st.subheader(f'{cols}의 데이터셋')
     tmp_df = df[df[group_by_option].isin([cols])][['title', 'imbd_votes', 'imbd_rating']]
     tmp_df.index = tmp_df.index+1
     tmp_df.index.name = 'rank'
     st.dataframe(tmp_df, width=700)
 
 
-st.divider()
+with st.sidebar:
+    st.markdown("<hr style='margin: 0px; border-bottom: 3px dashed rgba(49, 51, 63, 0.2);'>", unsafe_allow_html=True)
+    idx = st.text_input('검색하고 싶은 순위(rank)를 입력해주세요.')
+    if idx:
+        st.write(df.loc[int(idx)]['link'])
 
-idx = st.text_input('Enter the Index')
-if idx:
-    st.subheader('Info.')
-    st.write('Title) ' + df.loc[int(idx)]['title'])
-    st.write('Genre) ' + df.loc[int(idx)]['genre'])
-    st.write('Running time) ' + df.loc[int(idx)]['duration'])
-    st.write('Director) ' + df.loc[int(idx)]['director_name'])
-    st.write('Writer) ' + df.loc[int(idx)]['writer_name'])
-    st.write('Casts)')
-    st.write(df.loc[int(idx)]['cast_name'])
-    st.write('Storyline)')
-    st.write(df.loc[int(idx)]['storyline'])
-    st.write(':link: Go to detailed information')
-    st.write(df.loc[int(idx)]['link'])
-
-else:
-    st.empty()
+    else:
+        st.empty()
--- movie_pj/pages/Today's_movie.py
+++ movie_pj/pages/Today's_movie.py
@@ -4,79 +4,80 @@ import pandas as pd
 # 데이터 로드
 df = pd.read_csv('data/movie_eda.csv')
 
-### Header
-st.title(':thinking_face:오늘의 영화는?')
+### Sidebar
+with st.sidebar:
+    st.subheader("How to use")
+    st.markdown("""<span style='color:grey'>오늘은 또 어떤 영화를 감상해야 할지 고민인 당신!<span/><br/>
+    <span>지금 당신의 상황을 입력해주세요.<span/><br/>
+    <span>IMDB TOP 250 영화 데이터를 기반으로 추천해드립니다.:smile:<span/><br/>
+    <hr style='margin: 0px; border-bottom: 3px dashed rgba(49, 51, 63, 0.2);'>""", unsafe_allow_html=True)
 
-st.divider()
-
-st.markdown("""<span style='color:grey'>오늘은 또 어떤 영화를 감상해야 할지 고민인 당신!<span/><br/>
-<span>지금 당신의 상황을 입력해주세요.<span/><br/>
-<span>IMDB TOP 250 영화 데이터를 기반으로 추천해드립니다.:smile:<span/>""", unsafe_allow_html=True)
-
-### Main
 ## 질문 1: 시간 관련
 with st.sidebar:
+    st.subheader('Question')
     hour = st.radio(
-        '얼마나 시간이 있나요?',
-        ['1시간 정도 있어요', '영화는 2시간이 기본이죠', '하루종일 영화 보고싶어요']
+        'Q1. 얼마나 시간이 있나요?',
+        ['1시간 정도 있어요', '영화는 2시간 이상이 기본이죠']
     )
 # st.markdown('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
 if hour == '1시간 정도 있어요':
     running_time = '1h'
-    # st.write(running_time)
-elif hour == '영화는 2시간이 기본이죠':
-    running_time = '2h'
-    # st.write(running_time)
-else:
-    running_time = '3h'
-    # st.write(running_time)
+else: 
+    running_time = '2h|3h'
 
 duration_contains = df['duration'].str.contains(running_time)
 df_duration = df[duration_contains]
 
+
 ## 질문 2: 장르 관련
-genre_dic = {'웃고 싶은 날':'Comedy', '울고 싶은 날':'Drama'}
+genre_dic = {'웃고 싶은 날':'Comedy', '울고 싶은 날':'Drama', '가족과 함께하는 날':'Family', '상상의 나라로 고고':'Sci-Fi', '연애 하고 싶은 날':'Romance',
+             '동심의 세계로 고고':'Animation'}
 
 with st.sidebar:
-    what_day_lst = st.multiselect('오늘은 어떤 날이야?', ['웃고 싶은 날', '울고 싶은 날'])
+    what_day_lst = st.multiselect('Q2. 오늘은 어떤 날인가요? (2개 이상 선택해주세요.)', ['웃고 싶은 날', '울고 싶은 날', '가족과 함께하는 날',
+                                                       '상상의 나라로 고고', '연애 하고 싶은 날', '동심의 세계로 고고'])
     if what_day_lst:
-        for i in what_day_lst:
-           genre_contains = df['genre'].str.contains(genre_dic[i])
-        #    df_genre = df[duration_contains]
-        #    pd.concat()
-    else:
-        st.text(2)
+        tmp_lst = []
+        for genre in what_day_lst:
+            tmp_lst.append(genre_dic[genre])
 
-st.text(genre_contains)
+        genre_contains = df['genre'].str.contains("|".join(tmp_lst))
+        df_genre = df[genre_contains]
+
+    else:
+        # 임시로 df_genre를 만듦
+        df_genre = pd.DataFrame({'blank': [0,0]})
+        st.empty()
+
+## Main
+# Header
+st.title(':thinking_face:오늘의 영화는?')
+
 ## 추천 결과
 # 데이터 프레임 합치기
+if len(df_genre) == 2:
+    # 장르 선택을 하지 않으면 빈칸
+    st.subheader("사이드바에 있는 질문에 답해주세요.")
+else:
+    df_result = pd.concat([df_duration, df_genre])
+    # 랜덤으로 하나만 추출
+    movie_recommendation = df_result.sample(n=1)
+    idx = int(list(movie_recommendation.index)[0])
 
-# 랜덤으로 하나만 추출
-movie_recommendation = df_duration.sample(n=1)
-idx = int(list(movie_recommendation.index)[0])
-
-# 추천 영화 info
-st.subheader("오늘의 영화는 '" + df_duration.loc[int(idx)]['title'] + "' 입니다.")
-st.write('장르) ' + df_duration.loc[int(idx)]['genre'])
-st.write('러닝타임) ' + df_duration.loc[int(idx)]['duration'])
-st.write('감독) ' + df_duration.loc[int(idx)]['director_name'])
-st.write('작가) ' + df_duration.loc[int(idx)]['writer_name'])
-st.write('출연진)')
-st.write(df_duration.loc[int(idx)]['cast_name'])
-st.write('줄거리)')
-st.write(df_duration.loc[int(idx)]['storyline'])
-st.write(':link: Go to detailed information')
-st.write(df_duration.loc[int(idx)]['link'])
-
-# 만약 이미 봤다는 버튼을 누르면 데이터프레임에서 해당 컬럼 제거 후 다시 랜덤 추출
-# if st.button('이미 봤어요'):
-#     # 본 영화일 때
-#     df_duration.drop([idx], axis=0, inplace=True)
-# else:
-#     # 안본 영화일 때
-#     st.empty()
-
-# 더이상 추천할 영화가 없다면 다시 상황 입력 유도
-
-
+    # 추천 영화 info
+    st.markdown("""<style>
+    span.info_name{font-size: 20px; font-weight: bold; }
+    p{color: black; display:inline; font-size: 17px; margin-bottom:3px;}
+    div.box{ background-color:rgba(245, 200, 0, 0.26); padding:10px;}
+    <style/>""", unsafe_allow_html=True)
+    
+    st.header("'"+df_duration.loc[int(idx)]['title'] + "' 입니다.")
+    st.markdown(f"""<div class='box'>
+        <span class="info_name">장르)  <p>{df_duration.loc[int(idx)]['genre']}<p/><span/><br/>
+        <span class="info_name">러닝타임)  <p>{df_duration.loc[int(idx)]['duration']}<p/><span/><br/>
+        <span class="info_name">감독)  <p>{df_duration.loc[int(idx)]['director_name']}<p/><span/><br/>
+        <span class="info_name">작가)  <p>{df_duration.loc[int(idx)]['writer_name']}<p/><span/><br/>
+        <span class="info_name">줄거리)<br/><p>{df_duration.loc[int(idx)]['storyline']}<p/><span/><br/>
+        <span class="info_name"><a href="{df_duration.loc[int(idx)]['link']}">Go to Detailed Information!</a><span/>
+    <div/>""", unsafe_allow_html=True)
